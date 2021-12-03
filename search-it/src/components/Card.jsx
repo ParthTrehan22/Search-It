6a4eac8c63d028b08e11ce6fc3cef3fc99942a10
Parth Trehan <parthtrehan221997@gmail.com>
2021-12-04 02:01:09 +0530

    Added infinite scrolling.

--- search-it/src/components/Card.jsx
+++ search-it/src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import "../styles/Card.css"
+
+
 
 function Card(props) {
     return (
--- search-it/src/components/Header.jsx
+++ search-it/src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Header() {
     return (
         <div className="Header">
-            <nav class="navbar navbar-light bg-light">
-                <a class="navbar-brand" href="#">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9eF3AdDg-2a3ZuIuD-7voOtscmwtaoViPSA&usqp=CAU" width="30" height="30" class="d-inline-block align-top" alt=""/>
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9eF3AdDg-2a3ZuIuD-7voOtscmwtaoViPSA&usqp=CAU" width="30" height="30" className="d-inline-block align-top" alt=""/>
                     Search It!
                 </a>
             </nav>
