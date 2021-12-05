f93ca3261d5912d5279c3e98ddec37741f42fa23
Parth Trehan <parthtrehan221997@gmail.com>
2021-12-05 15:15:57 +0530

    Fixed Spinners and fetching

--- search-it/src/components/Header.jsx
+++ search-it/src/components/Header.jsx
@@ -1,105 +1,140 @@
-import React from 'react'
-import searchIcon from "../images/search.png"
-import "../styles/Header.css"
-import { useState, useEffect, useRef } from 'react'
-import { createApi } from "unsplash-js"
-import { debounce } from "lodash"
-import { BounceLoader } from 'react-spinners'
+import React, { useMemo, useCallback } from "react";
+import searchIcon from "../images/search.png";
+import "../styles/Header.css";
+import { useState, useEffect, useRef } from "react";
+import { createApi } from "unsplash-js";
+import { debounce } from "lodash";
+import { BeatLoader } from "react-spinners";
 
 const unsplash = createApi({
-    accessKey: "MZg0kz21lnxkcsvvhr-zOIC_5zJq3RDw59NB0FXrdSc"
-})
+  accessKey: "MZg0kz21lnxkcsvvhr-zOIC_5zJq3RDw59NB0FXrdSc",
+});
+
+const fetchImages = (query, page = 1) =>
+  new Promise((resolve, reject) => {
+    unsplash.search
+      .getPhotos({
+        query,
+        page,
+        perPage: 10,
+        w: 200,
+        fit: "max",
+      })
+      .then((result) => {
+        resolve(result.response.results.map((result) => result.urls.regular));
+      });
+  });
 
 function Header() {
-    const [search, setSearch] = useState("");
-    const searchRef = useRef(search)
-    const [images, setImages] = useState([])
-    const imagesRef = useRef(images)
-    const [fetching, setFetching] = useState(false)
-    const fetchingRef = useRef(fetching)
+  const [search, setSearch] = useState("");
+  const searchRef = useRef(search);
+  const [images, setImages] = useState([]);
+  const imagesRef = useRef(images);
+  const [fetching, setFetching] = useState(false);
+  const fetchingRef = useRef(fetching);
 
-    function handleSearch(e) {
-        setSearch(e.target.value)
+  const handleSearch = useCallback(
+    (e) => {
+      const searchQuery = e.target.value;
+      searchRef.current = searchQuery;
+      setSearch(searchQuery);
+    },
+    [setSearch]
+  );
+
+  const debouncedChangeHandler = useMemo(() => debounce(handleSearch, 1000), [
+    handleSearch,
+  ]);
+
+  const getUnsplashImages = useCallback(
+    (query, page) => {
+      setFetching(true);
+      fetchingRef.current = true;
+
+      return fetchImages(query, page).then((result) => {
+        setFetching(false);
+        fetchingRef.current = false;
+
+        return result;
+      });
+    },
+    [setFetching]
+  );
+
+  useEffect(() => {
+    if (search !== "") {
+      setImages([]);
+      getUnsplashImages(search, 1).then((images) => {
+        setImages(images);
+        imagesRef.current = images;
+      });
     }
+  }, [search, setImages, getUnsplashImages]);
 
-    function getUnsplashImages(query, page = 1) {
-        setFetching(true)
-        fetchingRef.current = fetching
-        return new Promise((resolve, reject) => {
-            unsplash.search.getPhotos({
-                query,
-                page,
-                perPage: 10,
-                w: 200,
-                fit: "max"
-            }).then(result => {
-                setFetching(false)
-                fetchingRef.current = fetching
-                resolve(result.response.results.map(result => result.urls.regular))
-            })
-        })
-    }
+  const handleScroll = useCallback(
+    (e) => {
+      const {
+        scrollHeight,
+        scrollTop,
+        clientHeight,
+      } = e.target.scrollingElement;
+      const isBottom = scrollHeight - scrollTop <= clientHeight;
+      if (isBottom && !fetchingRef.current) {
+        getUnsplashImages(
+          searchRef.current,
+          imagesRef.current.length / 10 + 1
+        ).then((newImages) => {
+          imagesRef.current = [...imagesRef.current, ...newImages];
+          setImages(imagesRef.current);
+        });
+      }
+    },
+    [getUnsplashImages, setImages]
+  );
 
-    useEffect(() => {
-        searchRef.current = search
-        if (search !== "") {
-            debounce(() => {
-                setImages([])
-                getUnsplashImages(search, 1).then(images => {
-                    setImages(images);
-                    imagesRef.current = images
-                })
-            }, 1000)();
-        }
-    }, [search])
+  useEffect(() => {
+    document.addEventListener("scroll", handleScroll, { passive: true });
+    return () => document.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
-    function handleScroll(e) {
-        const { scrollHeight, scrollTop, clientHeight } = e.target.scrollingElement;
-        const isBottom = scrollHeight - scrollTop <= clientHeight
-        if (isBottom && !fetchingRef.current) {
-            getUnsplashImages(searchRef.current, imagesRef.current.length / 10 + 1).then(
-                newImages => {
-                    imagesRef.current = [...imagesRef.current, ...newImages];
-                    setImages(imagesRef.current)
-                }
-            )
-        }
-    }
-    useEffect(() => {
-        document.addEventListener('scroll', handleScroll, { passive: true })
-        return () => document.removeEventListener("scroll", handleScroll)
-    }, [])
-
-    return (
-        <div className="app">
-            <div className="header">
-                <div className="left_header">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9eF3AdDg-2a3ZuIuD-7voOtscmwtaoViPSA&usqp=CAU" alt="logo" />
-                    <h2>Search It!</h2>
-                </div>
-                <div className="right_header">
-                    <div className="searchIcon">
-                        <img src={searchIcon} alt="search_logo" />
-                    </div>
-                    <div className="inputBar">
-                        <input type="search" placeholder="Enter your Search ..." onChange={handleSearch} />
-                    </div>
-                </div>
-            </div>
-            <div className="Main">
-                <div className="imagesContainer">
-                    {images.length > 0 && images.map(url => (
-                        <div className="image">
-                            <img src={url} alt="image" />
-                        </div>
-                    ))}
-                </div>
-                <div className="bounceLoader">
-                    {fetching && <BounceLoader speedMultiplier={5} color={"#000000"}></BounceLoader>}
-                </div>
-            </div>
+  return (
+    <div className="app">
+      <div className="header">
+        <div className="left_header">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9eF3AdDg-2a3ZuIuD-7voOtscmwtaoViPSA&usqp=CAU"
+            alt="logo"
+          />
+          <h2>Search It!</h2>
         </div>
-    )
+        <div className="right_header">
+          <div className="searchIcon">
+            <img src={searchIcon} alt="search_logo" />
+          </div>
+          <div className="inputBar">
+            <input
+              type="search"
+              placeholder="Search ..."
+              onChange={debouncedChangeHandler}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="imagesContainer">
+          {images.length > 0 &&
+            images.map((url) => (
+              <div key={url} className="image">
+                <img src={url} alt="image" />
+              </div>
+            ))}
+        </div>
+        <div className="loader">
+          {fetching && <BeatLoader color={"#000000"} />}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
